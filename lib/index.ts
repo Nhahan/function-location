6c31885f3cb6059e95a6bc71fcc4905b4dc89476
Nhahan <kisy324@naver.com>
2023-08-21 21:58:19 +0900

    refactor: `locate()`

--- lib/index.ts
+++ lib/index.ts
@@ -2,23 +2,6 @@ import { resolve } from 'path';
 
 export function locate(): string | undefined {
     const stackTrace = new Error().stack;
-    const filePathRegex = /at [^(]*\((.*\.js)/;
-    const matchedPath = (stackTrace?.match(filePathRegex) || [])[1];
-    return matchedPath && resolve(matchedPath);
+    const filePathMatch = stackTrace && stackTrace.match(/at\s+.*\s+\((.*):\d+:\d+\)/);
+    return filePathMatch ? filePathMatch[1] : undefined;
 }
-
-// function getFunctionLocation(entity?: Function | { new (...args: any[]): any }): string | undefined {
-//     const stackTrace = new Error().stack;
-//     const filePathRegex = /at [^(]*\((.*\.js)/;
-//     const matchedPath = (stackTrace?.match(filePathRegex) || [])[1];
-//
-//     if (entity) {
-//         if (typeof entity === 'function' || typeof entity === 'object') {
-//             return matchedPath && resolve(matchedPath);
-//         } else {
-//             throw new Error('Invalid parameter type. Expected a function or class.');
-//         }
-//     } else {
-//         return matchedPath && resolve(matchedPath);
-//     }
-// }
