bb46efac44bde1ecd590500ad1fc3ee07f1a3f12
Nhahan <kisy324@naver.com>
2023-08-25 00:03:24 +0900

    feat: `locateV8()`, but still temp code

--- lib/index.ts
+++ lib/index.ts
@@ -1,7 +1,20 @@
-import { resolve } from 'path';
+import {TestClass} from "../test/test-class";
 
-export function locate(): string | undefined {
-    const stackTrace = new Error().stack;
-    const filePathMatch = stackTrace && stackTrace.match(/at\s+.*\s+\((.*):\d+:\d+\)/);
-    return filePathMatch ? filePathMatch[1] : undefined;
+const {locateV8} = require('./native/build/Release/locate.node');
+
+function main() {
+    try {
+        const func = () => {};
+        const location = locateV8(TestClass);
+
+        if (location) {
+            console.log('Function location:', location);
+        } else {
+            console.log('Function location not available.');
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
 }
+
+main();
