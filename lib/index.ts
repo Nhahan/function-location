2f53a96f9c317b815bac71fb138daa509d18c9b4
Nhahan <kisy324@naver.com>
2023-08-27 23:39:20 +0900

    feat: `locateV8()`

--- lib/index.ts
+++ lib/index.ts
@@ -1,20 +1,5 @@
-import {TestClass} from "../test/test-class";
+const {locate} = require('./locate.node');
 
-const {locateV8} = require('../native/build/Release/locate.node');
-
-function main() {
-    try {
-        const func = () => {};
-        const location = locateV8(TestClass);
-
-        if (location) {
-            console.log('Function location:', location);
-        } else {
-            console.log('Function location not available.');
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    }
+export function locateV8(input: Function) {
+    return locate(input);
 }
-
-main();
