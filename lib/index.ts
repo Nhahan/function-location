bbd3b77065354e4d81a3d13667e371c1bbdf74fd
Nhahan <kisy324@naver.com>
2023-09-01 00:08:02 +0900

    todo: win32

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,12 @@
 import {TestClass} from "../test/test-class";
 
 const {locate} = require('./locate.node');
+// TODO: build on win32
+// const {locateWin} = require('./locate-win.node');
 
 export function locateV8(input: Function) {
+    // process.platform === 'win32' ? locate(input) : locate(locateWin(input));
+
     return locate(input);
 }
 
