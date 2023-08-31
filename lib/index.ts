24a4f9ab32088466809e02276d38812a1129d08f
Nhahan <kisy324@naver.com>
2023-08-31 22:56:08 +0900

    ref: napi

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,9 @@
+import {TestClass} from "../test/test-class";
+
 const {locate} = require('./locate.node');
 
 export function locateV8(input: Function) {
     return locate(input);
 }
+
+console.log(locateV8(TestClass));
--- native/locate.cc
+++ native/locate.cc
@@ -1,32 +1,43 @@
-#include <napi.h>
+#include <node_api.h>
 
-void GetFunctionLocation(const Napi::CallbackInfo& info) {
-  Napi::Env env = info.Env();
+napi_value GetFunctionLocation(napi_env env, napi_callback_info info) {
+  size_t argc = 1;
+  napi_value args[1];
+  napi_value jsReturnValue;
 
-  if (info.Length() < 1 || !info[0].IsFunction()) {
-    Napi::TypeError::New(env, "Function argument expected").ThrowAsJavaScriptException();
-    return;
+  napi_get_cb_info(env, info, &argc, args, NULL, NULL);
+
+  if (argc < 1) {
+    napi_throw_type_error(env, NULL, "Function argument expected");
+    return NULL;
   }
 
-  Napi::Function targetFunction = info[0].As<Napi::Function>();
+  napi_valuetype valueType;
+  napi_typeof(env, args[0], &valueType);
 
-  Napi::Value scriptOriginValue = targetFunction.Get("scriptOrigin").As<Napi::Object>().Get("resourceName");
-
-  if (!scriptOriginValue.IsUndefined()) {
-    Napi::Value scriptName = scriptOriginValue;
-    if (scriptName.IsString()) {
-      info.GetReturnValue().Set(scriptName);
-      return;
-    }
+  if (valueType != napi_function) {
+    napi_throw_type_error(env, NULL, "Function argument expected");
+    return NULL;
   }
 
-  info.GetReturnValue().SetUndefined();
+  napi_value targetFunction = args[0];
+
+  napi_value scriptOriginValue;
+  napi_get_named_property(env, targetFunction, "name", &scriptOriginValue);
+
+  napi_value scriptName;
+  napi_coerce_to_string(env, scriptOriginValue, &scriptName);
+
+  napi_get_undefined(env, &jsReturnValue);
+
+  return jsReturnValue;
 }
 
-Napi::Object Initialize(Napi::Env env, Napi::Object exports) {
-  exports.Set("locate", Napi::Function::New(env, GetFunctionLocation));
+napi_value Initialize(napi_env env, napi_value exports) {
+  napi_property_descriptor desc = {"locate", NULL, GetFunctionLocation, NULL, NULL, NULL, napi_default, NULL};
+  napi_define_properties(env, exports, 1, &desc);
+
   return exports;
 }
 
 NAPI_MODULE(NODE_GYP_MODULE_NAME, Initialize)
-
