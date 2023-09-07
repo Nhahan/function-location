426f7084aa3f346ffce18932b651412eea9a0422
Nhahan <kisy324@naver.com>
2023-09-07 22:37:29 +0900

    chore: remove log

--- lib/index.ts
+++ lib/index.ts
@@ -1,13 +1,8 @@
-import {TestClass} from "../test/index.spec";
-
 const {locate} = require('./locate.node');
 // TODO: build on win32
 // const {locateWin} = require('./locate-win.node');
 
-export function locateV8(input: Function) {
+export function locateV8(input?: Function) {
     // process.platform === 'win32' ? locate(input) : locate(locateWin(input));
-
     return locate(input);
 }
-
-console.log(locateV8(TestClass));
