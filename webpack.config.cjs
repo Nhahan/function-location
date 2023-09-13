ab1ba2026743a34f1efd95b8b71836217ee0b4da
Nhahan <kisy324@naver.com>
2023-09-13 21:30:59 +0900

    build: webpack settings

---
+++ webpack.config.cjs
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        index: './src/index.ts',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        clean: true,
+        library: {
+            name: 'FunctionLocation',
+            type: 'umd',
+        },
+        globalObject: 'this',
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.node']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: ['babel-loader', 'ts-loader'],
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    target: 'node',
+};
