1050500789d9f358c5110b7ad613b85f169eb08c
Nhahan <kisy324@naver.com>
2023-09-14 22:20:15 +0900

    fix: set webpack entry path properly

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     entry: {
-        index: './src/index.ts',
+        index: './lib/index.ts',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
