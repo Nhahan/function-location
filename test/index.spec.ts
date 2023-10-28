83e4b469f068736a27b1ef3e86b3d8689d5f667a
Nhahan <kisy324@naver.com>
2023-10-28 19:33:41 +0900

    test: `locateOs`

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -7,20 +7,32 @@ export function TestFunction() {
 }
 
 describe('locateV8', () => {
-    test('class path success', () => {
+        test('class path success', () => {
         const classPath = locateV8(TestClass);
         expect(classPath).toEqual(__filename);
     });
+
     test('class path fail', () => {
         const classPath = locateV8(TestClass);
         expect(classPath).not.toEqual(__dirname);
     });
+
     test('function path', () => {
         const functionPath = locateV8(TestFunction);
         expect(functionPath).toEqual(__filename);
     });
+
     test('function path', () => {
         const functionPath = locateV8(TestFunction);
         expect(functionPath).not.toEqual(__dirname);
     });
+
+    test('should call right native addon', () => {
+        const locateOs = process.platform === 'win32' ? './locate-win.node' : './locate.node';
+        if (locateOs === './locate-win.node') {
+            expect(locateOs).toEqual('./locate-win.node');
+        } else {
+            expect(locateOs).toEqual('./locate.node');
+        }
+    })
 });
